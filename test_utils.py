e2934f11926e271a443cf5ba6f66887afe0765b3
Robert <robert.kwiatkowski01@gmail.com>
2023-03-14 09:00:02 +0100

    RSI

--- test_utils.py
+++ test_utils.py
@@ -1,5 +1,7 @@
-import pytest
+import math
 import numpy as np
+import pytest
+
 import utils
 
 
@@ -7,4 +9,14 @@ def test_macd():
     data = np.array([1, 1, 1, 1])
     result = utils.macd(data)
     expected = np.array([0, 0, 0, 0])
+
     assert all([a == b for a, b in zip(result, expected)])
+
+
+def test_rsi():
+    data = np.arange(0, 4, 1)
+    result = utils.rsi(data, 3)
+    expected = np.array([np.NaN, np.NaN, 100.0, 100.0])
+
+    assert all([math.isnan(a) for a in result[:1]])  # check for NaN
+    assert all([pytest.approx(a) == b for a, b in zip(result[2:], expected[2:])])  # check for values
--- utils.py
+++ utils.py
@@ -26,3 +26,22 @@ def macd(data: Union[np.ndarray, pd.DataFrame], fast: int = 12, slow: int = 28):
     exp1 = df.ewm(span=fast, adjust=False).mean()
     exp2 = df.ewm(span=slow, adjust=False).mean()
     return np.array(exp1 - exp2)
+
+
+def rsi(data: Union[np.ndarray, pd.DataFrame], window: int):
+    try:
+        if isinstance(data, np.ndarray):
+            df = pd.DataFrame(data)
+        else:
+            df = data.copy()
+    except TypeError:
+        print("Wrong data input type")
+
+    change = df.diff()
+    change_up = change[change>0].fillna(0)
+    change_down = change[change<0].fillna(0)
+    avg_up = change_up.rolling(window).mean()
+    avg_down = change_down.rolling(window).mean().abs()
+    rsi_result = 100 * avg_up / (avg_up + avg_down)
+    return rsi_result.to_numpy().flatten()
+
